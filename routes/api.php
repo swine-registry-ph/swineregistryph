98c2d8f01a85cfe64b6f17a0c7b8cb6812bf27f5
Evan Tolorio <evantolorio@gmail.com>
2018-02-05 17:50:24 +0800

    Install and setup Laravel Passport for Client Credentials Grant tokens

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::group(['prefix' => 'v1'], function() {
+Route::group(['prefix' => 'v1', 'middleware' => 'client'], function() {
     Route::group(['prefix' => 'breeders'], function() {
         Route::get('/', 'BreederResourceController@index');
         Route::get('/{farm}', 'BreederResourceController@show');
